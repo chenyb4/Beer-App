a1e3a1bfb1cb1a844cfd533ba6640b9ba67e8b86
Wouter Baltus <439831@student.saxion.nl>
2024-05-31 15:26:47 +0200

    When no credit data is available, the update credit will try to create a new credit. If this goes wrong it will give an appropriate error message. Additionally, made sure that the ID of the credit does not influence the code unless it goes over number 100.

--- Backend/services/CreditService.js
+++ Backend/services/CreditService.js
@@ -9,20 +9,42 @@ exports.updateCredit = async (default_amount, price) => {
         throw new Error('Missing required fields or no update data provided');
     }
 
-    try {
-        return await db.Credit.update(
-            {
-                default_amount,
-                price,
-            },
-            {
-                where: {
-                    id: 2
+    const credit = await this.getCredit()
+    if(!credit) {
+        return await this.createCredit(default_amount, price, "No credit data available. Creating new Credit => ")
+    } else {
+        let ids = [1,100];
+        try {
+            return await db.Credit.update(
+                {
+                    default_amount,
+                    price,
+                },
+                {
+                    where: {
+                        id: ids
+                    }
                 }
-            }
-        );
+            );
+        } catch (err) {
+            console.error(err);
+            throw new Error('Failed to update credit');
+        }
+    }
+
+
+
+};
+
+exports.createCredit = async (default_amount, price, additionalMessage = '') => {
+    if (!default_amount || !price) {
+        throw new Error(additionalMessage + 'Missing required fields');
+    }
+
+    try {
+        return await db.Credit.create({default_amount, price});
     } catch (err) {
         console.error(err);
-        throw new Error('Failed to update credit');
+        throw new Error(additionalMessage + 'Failed to create credit');
     }
-};
+}
