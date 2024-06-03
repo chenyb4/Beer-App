3c1ae67ed50db8abba60804c682d2b63a1109721
Marnixkls <483545@student.saxion.nl>
2024-06-03 09:40:41 +0200

    Drinks selecting now works properly

--- Frontend/src/lib/components/DrinkSearchBar.svelte
+++ Frontend/src/lib/components/DrinkSearchBar.svelte
@@ -1,93 +1,94 @@
 <script>
-    // @ts-nocheck
+  // @ts-nocheck
 
-    import { t } from "$lib/translations/index.js";
-    import { onMount } from "svelte";
+  import { t } from "$lib/translations/index.js";
+  import { createEventDispatcher, onMount } from "svelte";
 
-    export let filteredProductsOptions = [];
-    export let value = "";
-    export let selectedProduct;
+  export let filteredProductsOptions = [];
+  export let value = "";
+  export let selectedProduct;
 
-    let products;
-    let errorMessage = "";
+  let products = [];
+  let env = import.meta.env;
 
-    let env = import.meta.env;
+  const dispatch = createEventDispatcher();
 
-    function showProductOptions() {
-        document.getElementById("productOptions").style.display = "block";
+  function showProductOptions() {
+    document.getElementById("productOptions").style.display = "block";
+  }
+
+  async function loadProducts() {
+    try {
+      const response = await fetch(
+        "http://" + env.VITE_APIURL + ":" + env.VITE_APIPORT + "/products"
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      let jsonData = await response.json();
+      return jsonData.data;
+    } catch (error) {
+      console.error("Failed to fetch user data:", error);
     }
+  }
 
-    async function loadProducts() {
-        try {
-            const response = await fetch(
-                "http://" +
-                    env.VITE_APIURL +
-                    ":" +
-                    env.VITE_APIPORT +
-                    "/products",
-            );
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            products = await response.json();
-            console.log(products);
-            errorMessage = "";
-        } catch (error) {
-            console.error("Failed to fetch user data:", error);
-            errorMessage = "Failed to fetch user data. Please try again later.";
-        }
+  function filterProductOptions() {
+    if (value === "") {
+      filteredProductsOptions = [];
+    } else {
+      filteredProductsOptions = products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(value.toLowerCase()) ||
+          product.EAN.includes(value)
+      );
     }
+    showProductOptions();
+  }
 
-    function filterProductOptions() {
-        filteredProductsOptions = products.filter(
-            (product) =>
-                product.name.toLowerCase().match(value.toLowerCase()) ||
-                product.EAN.match(value),
-        );
-        if (value === "") {
-            filteredProductsOptions = [];
-        }
+  function selectProduct(product) {
+    selectedProduct = product;
+    document.getElementById("productOptions").style.display = "none";
+    dispatch("selectProduct", { product });
+    resetInputField();
+  }
+
+  function resetInputField() {
+    value = "";
+    filteredProductsOptions = [];
+    document.getElementById("inputSearchBar").focus();
+  }
+
+  function selectProductWithEnter(event) {
+    if (event.key === "Enter") {
+      if (filteredProductsOptions.length > 0) {
+        selectProduct(filteredProductsOptions[0]);
+      }
     }
+  }
 
-    function selectProduct(product) {
-        selectedProduct = product;
-        value = product.name;
-        document.getElementById("productOptions").style.display = "none";
-    }
-
-    function selectProductWithEnter(event) {
-        if (event.key === "Enter") {
-            if (filteredProductsOptions.length > 0) {
-                selectProduct(filteredProductsOptions[0]);
-            }
-        }
-    }
-
-    onMount(() => {
-        loadProducts();
-    });
+  onMount(async () => {
+    products = await loadProducts();
+  });
 </script>
 
 <div id="searchBar" class="flex flex-col">
-    <label class="pb-0.5" for="inputSearchBar"
-        >{$t("drinks.drinkScanner")}</label
-    >
-    <input
-        class="dark:bg-dark-800 border-none rounded-lg"
-        type="text"
-        id="inputSearchBar"
-        bind:value
-        on:focus={showProductOptions}
-        on:input={filterProductOptions}
-        on:keydown={selectProductWithEnter}
-    />
-    <div id="productOptions" class="block">
-        {#each filteredProductsOptions as product}
-            <button
-                class="border border-dark-300 w-full"
-                on:click={selectProduct(product)}>{product.name}</button
-            >
-        {/each}
-    </div>
+  <label class="pb-0.5" for="inputSearchBar">{$t("drinks.drinkScanner")}</label>
+  <input
+    class="dark:bg-dark-800 border-none rounded-lg"
+    type="text"
+    id="inputSearchBar"
+    bind:value
+    on:focus={showProductOptions}
+    on:input={filterProductOptions}
+    on:keydown={selectProductWithEnter}
+  />
+  <div id="productOptions" class="block">
+    {#each filteredProductsOptions as product}
+      <button
+        class="border border-dark-300 w-full"
+        on:click={() => selectProduct(product)}>{product.name}</button
+      >
+    {/each}
+  </div>
 </div>
--- Frontend/src/routes/drinks/+page.svelte
+++ Frontend/src/routes/drinks/+page.svelte
@@ -1,99 +1,106 @@
 <script>
-    import InputField from "$lib/components/InputField.svelte";
-    import { t } from "$lib/translations/index.js";
-    import { onMount } from "svelte";
-    import DrinkSearchBar from "$lib/components/DrinkSearchBar.svelte";
-    import StudentIdentifier from "$lib/components/StudentIdentifier.svelte";
-    import CtaButton from "$lib/components/CtaButton.svelte";
-    import { TrashBinSolid } from "flowbite-svelte-icons";
+  // @ts-nocheck
 
-    let ref;
-    let identifiedUser;
-    let identifier = "";
-    let userName = "";
-    let drinksScanner = "";
-    let product;
+  import InputField from "$lib/components/InputField.svelte";
+  import { t } from "$lib/translations/index.js";
+  import { onMount } from "svelte";
+  import DrinkSearchBar from "$lib/components/DrinkSearchBar.svelte";
+  import StudentIdentifier from "$lib/components/StudentIdentifier.svelte";
+  import CtaButton from "$lib/components/CtaButton.svelte";
+  import { TrashBinSolid } from "flowbite-svelte-icons";
 
-    $: product, console.log(product);
+  let ref;
+  let identifiedUser;
+  let identifier;
+  let userName;
+  let drinksScanner;
+  let product;
+  let selectedProducts = [];
 
-    onMount(() => {
-        ref.focus();
-    });
+  $: console.log(selectedProducts);
+
+  function handleSelectProduct(event) {
+    const selectedProduct = event.detail.product;
+    selectedProducts = [...selectedProducts, selectedProduct];
+  }
+
+  onMount(() => {
+    ref.focus();
+  });
 </script>
 
 <body class="bg-dark-800 w-screen h-screen">
-    <div class="w-full h-full">
-        <div class="bg-dark-900 m-4 rounded-xl">
-            <h2 class="p-4 font-bold">{$t("drinks.title")}</h2>
-            <div class="flex flex-col mx-4">
-                <div class="flex flex-row justify-between">
-                    <StudentIdentifier
-                        bind:identifiedUser
-                        bind:identifier
-                        bind:userName
-                        bind:ref
-                    />
-                    {#if identifiedUser}
-                        <div
-                            class="bg-dark-300 w-64 p-4 rounded-2xl flex-col items-center"
-                        >
-                            <h4>Amount of credits left:</h4>
-                            <p class="flex justify-center text-4xl">
-                                {identifiedUser.credits}
-                            </p>
-                        </div>
-                    {/if}
-                </div>
-                <div class=" w-72">
-                    <DrinkSearchBar
-                        bind:value={drinksScanner}
-                        bind:selectedProduct={product}
-                    ></DrinkSearchBar>
+  <div class="w-full h-full">
+    <div class="bg-dark-900 m-4 rounded-xl">
+      <h2 class="p-4 font-bold">{$t("drinks.title")}</h2>
+      <div class="flex flex-col mx-4">
+        <div class="flex flex-row justify-between">
+          <StudentIdentifier
+            bind:identifiedUser
+            bind:identifier
+            bind:userName
+            bind:ref
+          />
+          {#if identifiedUser}
+            <div class="bg-dark-300 w-64 p-4 rounded-2xl flex-col items-center">
+              <h4>Amount of credits left:</h4>
+              <p class="flex justify-center text-4xl">
+                {identifiedUser.credits}
+              </p>
+            </div>
+          {/if}
+        </div>
+        <div class=" w-72">
+          <DrinkSearchBar
+            bind:value={drinksScanner}
+            bind:selectedProduct={product}
+            on:selectProduct={handleSelectProduct}
+          />
+        </div>
+        <div
+          class="bg-dark-800 w-full h-full my-6 flex flex-col min-h-80 rounded-xl"
+        >
+          <div class="px-4 py-2">
+            <div class="grid grid-cols-6 gap-2 font-bold">
+              <div class="col-span-3">Product</div>
+              <div
+                class="col-span-2 flex items-center justify-center text-center"
+              >
+                Amount
+              </div>
+              <div
+                class="col-span-1 flex items-center justify-center text-center"
+              >
+                Remove
+              </div>
+            </div>
+
+            {#each selectedProducts as product}
+              <div
+                class="grid grid-cols-6 gap-2 mt-2 bg-dark-900 rounded-xl p-4"
+              >
+                <div class="col-span-3">
+                  {product.name}
                 </div>
                 <div
-                    class="bg-dark-800 w-full h-full my-6 flex flex-col min-h-80 rounded-xl"
+                  class="col-span-2 flex justify-center text-center items-center"
                 >
-                    <div class="px-4 py-2">
-                        <div class="grid grid-cols-6 gap-2 font-bold">
-                            <div class="col-span-3">Product</div>
-                            <div
-                                class="col-span-2 flex items-center justify-center text-center"
-                            >
-                                Amount
-                            </div>
-                            <div
-                                class="col-span-1 flex items-center justify-center text-center"
-                            >
-                                Remove
-                            </div>
-                        </div>
-
-                        {#if product}
-                            <div
-                                class="grid grid-cols-6 gap-2 mt-2 bg-dark-900 rounded-xl p-4"
-                            >
-                                <div class="col-span-3">
-                                    {product.name}
-                                </div>
-                                <div
-                                    class="col-span-2 flex justify-center text-center items-center"
-                                >
-                                    1
-                                </div>
-                                <div
-                                    class="col-span-1 flex justify-center text-center items-center"
-                                >
-                                    <svelte:component
-                                        this={TrashBinSolid}
-                                        class="text-light-p_foreground h-full"
-                                    />
-                                </div>
-                            </div>
-                        {/if}
-                    </div>
+                  1
                 </div>
-                <CtaButton captionText="Submit"></CtaButton>
-            </div>
+                <div
+                  class="col-span-1 flex justify-center text-center items-center"
+                >
+                  <svelte:component
+                    this={TrashBinSolid}
+                    class="text-light-p_foreground h-full"
+                  />
+                </div>
+              </div>
+            {/each}
+          </div>
         </div>
+        <CtaButton captionText="Submit"></CtaButton>
+      </div>
     </div>
+  </div>
 </body>
