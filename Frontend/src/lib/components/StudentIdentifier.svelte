6149aa28b5e3ca32271a69e58aa2a6530f02d4d5
Marnixkls <483545@student.saxion.nl>
2024-06-03 13:40:20 +0200

    Identifier gebruikt nu de QR identifier

--- Frontend/src/lib/components/StudentIdentifier.svelte
+++ Frontend/src/lib/components/StudentIdentifier.svelte
@@ -1,81 +1,57 @@
 <script>
-    // @ts-nocheck
+  // @ts-nocheck
 
-    import { t } from "$lib/translations/index.js";
+  import { getUserByQRIdentifier } from "$lib/service/users";
+  import { t } from "$lib/translations/index.js";
 
-    export let identifier = "";
-    export let userName = "";
-    export let ref = "";
-    export let identifiedUser;
-    let errorMessage = "";
-    let user;
+  export let identifier = "";
+  export let userName = "";
+  export let ref = "";
+  export let identifiedUser;
+  let errorMessage = "";
 
-    let env = import.meta.env;
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        matchIdentifierToStudent(identifier);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    errorMessage = "";
+    try {
+      identifiedUser = await getUserByQRIdentifier(identifier);
+      userName = identifiedUser.username;
+    } catch (error) {
+      errorMessage = $t("drinks.errorMessage");
+      userName = "";
     }
-
-    async function matchIdentifierToStudent(identifier) {
-        try {
-            const response = await fetch(
-                "http://" +
-                    env.VITE_APIURL +
-                    ":" +
-                    env.VITE_APIPORT +
-                    "/users?id=" +
-                    identifier,
-            );
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            user = await response.json();
-            identifiedUser = user;
-            userName = user.username;
-            errorMessage = "";
-        } catch (error) {
-            console.error("Failed to fetch user data:", error);
-            errorMessage = "Failed to fetch user data. Please try again later.";
-            userName = "";
-            identifiedUser = "";
-        }
-    }
+  }
 </script>
 
 <form on:submit={handleSubmit}>
-    <div class="flex flex-col">
-        <label class="pb-0.5" for="identifier_input"
-            >{$t("drinks.identifier")}</label
-        >
-        <div class="flex flex-row items-center">
-            <input
-                class="bg-white-200 text-black rounded-lg border-none dark:bg-dark-800 dark:text-white focus:outline-0"
-                id="identifier_input"
-                type="text"
-                bind:value={identifier}
-                bind:this={ref}
-            />
-            <button
-                class="bg-dark-200 rounded-2xl ml-2 px-4 py-2"
-                type="submit"
-            >
-                Submit
-            </button>
-            {#if errorMessage}
-                <p class="text-red-400 px-4">{errorMessage}</p>
-            {/if}
-        </div>
-        <label class="pb-0.5" for="studentNumber_input"
-            >{$t("drinks.studentNumber")}</label
-        >
-        <input
-            disabled
-            class="bg-white-200 text-black rounded-lg border-none dark:bg-dark-800 dark:text-white focus:outline-0 opacity-55 max-w-72"
-            id="studentNumber_input"
-            type="text"
-            bind:value={userName}
-        />
+  <div class="flex flex-col">
+    <label class="pb-0.5" for="identifier_input"
+      >{$t("drinks.identifier")}</label
+    >
+    <div class="flex flex-row items-center">
+      <input
+        class="bg-white-200 text-black rounded-lg border-none dark:bg-dark-800 dark:text-white focus:outline-0"
+        id="identifier_input"
+        type="text"
+        bind:value={identifier}
+        bind:this={ref}
+      />
+      <button class="bg-dark-200 rounded-2xl ml-2 px-4 py-2" type="submit">
+        Submit
+      </button>
+      {#if errorMessage}
+        <p class="text-red-400 px-4">{errorMessage}</p>
+      {/if}
     </div>
+    <label class="pb-0.5" for="studentNumber_input"
+      >{$t("drinks.studentNumber")}</label
+    >
+    <input
+      disabled
+      class="bg-white-200 text-black rounded-lg border-none dark:bg-dark-800 dark:text-white focus:outline-0 opacity-55 max-w-72"
+      id="studentNumber_input"
+      type="text"
+      bind:value={userName}
+    />
+  </div>
 </form>
