9704a366f4fd523e2f38541a2bd253a57796eb7b
Marnixkls <483545@student.saxion.nl>
2024-06-06 00:24:08 +0200

    initial commit

--- Frontend/src/lib/service/inventory.js
+++ Frontend/src/lib/service/inventory.js
@@ -15,3 +15,39 @@ export async function loadProducts() {
       console.error("Failed to fetch user data:", error);
     }
   }
+
+  export async function getProducts(page = 1, pageSize = 10) {
+    try {
+        const response = await fetch(`http://${env.VITE_APIURL}:${env.VITE_APIPORT}/products?pageSize=${pageSize}&page=${page}`, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "GET",
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to fetch products data:", error);
+    }
+}
+
+export async function createProduct(name, price_in_credits, amount_in_stock, EAN) {
+  try {
+    const response = await fetch(`http://${env.VITE_APIURL}:${env.VITE_APIPORT}/products`, {
+      headers: {
+          "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({name, price_in_credits, amount_in_stock, EAN})
+  });
+
+  if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json();
+  } catch (error) {
+    console.error("Failed to create product", error);
+  }
+}
--- Frontend/src/routes/inventory_management/+page.svelte
+++ Frontend/src/routes/inventory_management/+page.svelte
@@ -1,30 +1,60 @@
 <script>
-  import CtaButton from "$lib/components/CtaButton.svelte";
+import CtaButton from "$lib/components/CtaButton.svelte";
 import TableCell from "$lib/components/table/TableCell.svelte";
 import TableHeader from "$lib/components/table/TableHeader.svelte";
-  import TablePage from "$lib/components/table/TablePage.svelte";
+import TablePage from "$lib/components/table/TablePage.svelte";
+  import { getProducts } from "$lib/service/inventory";
 import {t} from "$lib/translations/index.js";
-  import { TableBody, TableBodyRow } from "flowbite-svelte";
+import { TableBody, TableBodyRow } from "flowbite-svelte";
+import { CirclePlusSolid } from "flowbite-svelte-icons";
+import CreateProduct from "$lib/components/CreateProduct.svelte";
 
-  let pages = 2;
+
+ /** @type {import('./$types').PageData} */
+  export let data;
+  $: products = data.products.data;
+
+  const pages = Math.ceil(data.products.meta.total / data.products.meta.page_size);
   let currentPage = 1;
+
+  const iconStyle = "hover:cursor-pointer hover:bg-light-p_foreground dark:hover:bg-dark-p_foreground rounded h-6 w-6";
+
+  let openCreateProductDialog = false;
+
+  async function changeProducts(page = 1, pageSize = 10) {
+        const response = await getProducts(page, pageSize);
+        products = response.data;
+        currentPage = page;
+    }
+
+function handleOpenCreateProductDialog(){
+        console.log("hi");
+        openCreateProductDialog = false;
+        openCreateProductDialog = true;
+}
 </script>
 
-<body class="h-full w-full">
-            <TablePage {pages} {currentPage} title={$t("inventory_management.title")}>
-            <TableHeader headerValues= {[
-                "Name",
-                "Stock",
-                ""
-            ]}></TableHeader>
-            <TableBody>
-                <TableBodyRow>
-                <TableCell position="first">Heineken</TableCell>
-                <TableCell position="middle">24</TableCell>
-                <TableCell position="last">+</TableCell>
-            </TableBodyRow>
-            </TableBody>
-            <div class="flex">
-            <CtaButton captionText="Add new item"></CtaButton></div></TablePage>
-            
-</body>
+<div class="fixed bottom-12 right-12 z-50">
+    <CtaButton
+            captionText={$t("inventory_management.addProduct")}
+            onCTAButtonClickFn={handleOpenCreateProductDialog}
+    />
+</div>
+<CreateProduct openCreateProductDialog={openCreateProductDialog} onClose={changeProducts}/>
+<TablePage {pages} {currentPage} changeData={changeProducts} title={$t("inventory_management.title")}>
+    <TableHeader headerValues= {[
+    $t("inventory_management.name"),
+    $t("inventory_management.stock"),
+    ""
+    ]}></TableHeader>
+    <TableBody>
+        {#each products as product}
+        <TableBodyRow>
+            <TableCell position="first">{product.name}</TableCell>
+            <TableCell position="middle">{product.amount_in_stock}</TableCell>
+            <TableCell position="last"><button><CirclePlusSolid class="{iconStyle}"></CirclePlusSolid></button></TableCell>
+        </TableBodyRow>
+        {/each}
+    </TableBody>
+</TablePage>
+        
