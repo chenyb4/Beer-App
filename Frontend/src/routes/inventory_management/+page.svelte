f292845fc6514c80e0e402a9508effa7cbbcf678
Marnixkls <483545@student.saxion.nl>
2024-06-05 10:24:34 +0200

    Setted up basic layout for the page

--- Frontend/src/routes/inventory_management/+page.svelte
+++ Frontend/src/routes/inventory_management/+page.svelte
@@ -1,9 +1,26 @@
 <script>
+  import TableCell from "$lib/components/table/TableCell.svelte";
+import TableHeader from "$lib/components/table/TableHeader.svelte";
+  import TablePage from "$lib/components/table/TablePage.svelte";
 import {t} from "$lib/translations/index.js";
+  import { TableBody, TableBodyRow } from "flowbite-svelte";
+
+  let pages = 2;
+  let currentPage = 1;
 </script>
 
-<div>
-    <!-- you can use `placeholders` and `modifiers` in your definitions (see docs) -->
-    <h2>{$t('drinks.title')}</h2>
-    <p>{$t('drinks.text')}</p>
-</div>
+<body class="h-full w-full">
+            <TablePage {pages} {currentPage} title={$t("inventory_management.title")}>
+            <TableHeader headerValues= {[
+                "Name",
+                "Stock",
+                ""
+            ]}></TableHeader>
+            <TableBody>
+                <TableBodyRow>
+                <TableCell position="first">Heineken</TableCell>
+                <TableCell position="middle">24</TableCell>
+                <TableCell position="last">+</TableCell>
+            </TableBodyRow>
+            </TableBody></TablePage>
+</body>
