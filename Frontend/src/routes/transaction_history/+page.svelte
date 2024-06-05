d61b17823ba026448f0a372a8e23e4b8a532ac59
Yibing Chen <444980@student.saxion.nl>
2024-06-05 16:12:02 +0200

    transaction history page utilizing custom components.

--- Frontend/src/routes/transaction_history/+page.svelte
+++ Frontend/src/routes/transaction_history/+page.svelte
@@ -1,9 +1,77 @@
 <script>
-import {t} from "$lib/translations/index.js";
+    import { t } from "$lib/translations/index.js";
+    import {TableBody, TableBodyRow} from "flowbite-svelte";
+    import TableHeader from "$lib/components/table/TableHeader.svelte";
+    import CtaButton from "$lib/components/CtaButton.svelte";
+    import TableCell from "$lib/components/table/TableCell.svelte";
+    import TablePage from "$lib/components/table/TablePage.svelte";
+
+    let dummyData = [
+        {
+            studentNumber: 510893,
+            credits: 60,
+            barkeeper: "Danfny",
+            date: "2024-6-1"
+        },
+        {
+            studentNumber: 510893,
+            credits: 60,
+            barkeeper: "Danny",
+            date: "2024-6-1"
+        },
+        {
+            studentNumber: 510893,
+            credits: 60,
+            barkeeper: "Danny",
+            date: "2024-6-1"
+        },
+        {
+            studentNumber: 510893,
+            credits: 60,
+            barkeeper: "Danny",
+            date: "2024-6-1"
+        }
+    ];
+
+    let pages=5;
+    let currentPage=2;
+
+
 </script>
 
-<div>
-    <!-- you can use `placeholders` and `modifiers` in your definitions (see docs) -->
-    <h2>{$t('drinks.title')}</h2>
-    <p>{$t('drinks.text')}</p>
-</div>
+
+
+
+
+
+
+<body class="h-full w-full">
+<TablePage {pages} {currentPage} title={$t("transaction_history.title")}>
+    <TableHeader headerValues= {[
+                $t('transaction_history.studentNumber'),
+                $t('transaction_history.credits'),
+                $t('transaction_history.barkeeper'),
+                $t('transaction_history.date')
+            ]}>
+    </TableHeader>
+
+
+    <TableBody>
+        {#each dummyData as entry}
+        <TableBodyRow>
+            <TableCell position="first">{entry.studentNumber}</TableCell>
+            <TableCell position="middle">{entry.credits}</TableCell>
+            <TableCell position="last">{entry.barkeeper}</TableCell>
+            <TableCell position="last">{entry.date}</TableCell>
+        </TableBodyRow>
+        {/each}
+    </TableBody>
+</TablePage>
+</body>
+
+
+
+
+
+
+
