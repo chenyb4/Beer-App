993b63dd2d335e54db875e4133c47326a3065422
Yibing Chen <444980@student.saxion.nl>
2024-06-07 17:06:30 +0200

    adding a service function for credits increment. identifier instead of student number

--- Frontend/src/lib/service/credits.js
+++ Frontend/src/lib/service/credits.js
@@ -32,3 +32,20 @@ export async function getDefaultCredits(){
         console.error("Failed to fetch products data:", error);
     }
 }
+
+export async function addCreditsForAUser(id,creditsToAdd){
+    try {
+        const response = await fetch(`http://${env.VITE_APIURL}:${env.VITE_APIPORT}/users?id=${id}`, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "GET",
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to fetch products data:", error);
+    }
+}
