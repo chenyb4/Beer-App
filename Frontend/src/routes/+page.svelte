cb9f2c628ffed399c57d6e04c5649524e3938361
Marnixkls <483545@student.saxion.nl>
2024-06-07 13:56:07 +0200

    final commit

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,22 +1,23 @@
 <script>
-    import ClickableCard from "$lib/components/cards/ClickableCard.svelte";
+  import ClickableCard from "$lib/components/cards/ClickableCard.svelte";
 
-    import {
-        CartSolid,
-        CashSolid,
-        ClipboardListSolid,
-        FileInvoiceSolid,
-        UsersGroupSolid,
-        WalletSolid
-    } from 'flowbite-svelte-icons';
+  import {
+    CartSolid,
+    CashSolid,
+    ClipboardListSolid,
+    FileInvoiceSolid,
+    UsersGroupSolid,
+    WalletSolid,
+  } from "flowbite-svelte-icons";
 </script>
+
 <div class="flex-1 overflow-y-auto">
-    <div class="flex flex-wrap justify-center">
-        <ClickableCard path="credits" icon="{WalletSolid}"/>
-        <ClickableCard path="drinks" icon="{CartSolid}"/>
-        <ClickableCard path="inventory_management" icon="{ClipboardListSolid}"/>
-        <ClickableCard path="administration" icon="{UsersGroupSolid}"/>
-        <ClickableCard path="transaction_history" icon="{CashSolid}"/>
-        <ClickableCard path="inventory_history" icon="{FileInvoiceSolid}"/>
-    </div>
+  <div class="flex flex-wrap justify-center">
+    <ClickableCard path="credits" icon={WalletSolid} />
+    <ClickableCard path="drinks" icon={CartSolid} />
+    <ClickableCard path="inventory_management" icon={ClipboardListSolid} />
+    <ClickableCard path="administration" icon={UsersGroupSolid} />
+    <ClickableCard path="transaction_history" icon={CashSolid} />
+    <ClickableCard path="inventory_history" icon={FileInvoiceSolid} />
+  </div>
 </div>
