676d996f7ea431fe6b55cd2e19bca693631dc5db
Marnixkls <483545@student.saxion.nl>
2024-06-11 15:28:28 +0200

    Finished the layout logic yet to be implemented

--- Frontend/src/routes/login/+page.svelte
+++ Frontend/src/routes/login/+page.svelte
@@ -1,31 +1,37 @@
 <script>
-  // Your login logic here
+  import { Button, Input, Label } from "flowbite-svelte";
+
+  let username = "";
+  let password = "";
 </script>
 
 <div class="content flex">
-  <div class="m-auto h-[50%] w-[45%] bg-dark-900 rounded-xl flex">
-    <div class="h-full flex-1 flex flex-col justify-center px-6">
-      <label for="username" class="text-white mb-2">Username:</label>
-      <input
+  <div class="m-auto h-[50%] w-[45%] bg-dark-800 rounded-xl flex">
+    <div class="h-full flex-1 flex flex-col justify-start p-6">
+      <h1 class="text-white text-2xl mb-10">Welcome back!</h1>
+      <Label for="username" class="text-white mb-2">Username:</Label>
+      <Input
+        required
         type="text"
         id="username"
-        name="username"
-        class="px-4 py-2 rounded-lg bg-gray-800 text-white mb-4"
-      />
-      <label for="password" class="text-white mb-2">Password:</label>
-      <input
+        class="bg-dark-900 border-none mb-4"
+        bind:value={username}
+      ></Input>
+      <Label for="password" class="text-white mb-2">Password:</Label>
+      <Input
+        required
         type="password"
         id="password"
-        name="password"
-        class="px-4 py-2 rounded-lg bg-gray-800 text-white mb-4"
-      />
-      <button
-        type="submit"
-        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        >Login</button
+        class="bg-dark-900 border-none mb-8"
+        bind:value={password}
+      ></Input>
+      <Button type="submit" class="bg-dark-p_foreground p-4 rounded-full"
+        >LOGIN</Button
       >
     </div>
-    <div class="h-[90%] w-0.5 bg-dark-800 flex items-center my-3"></div>
+
+    <div class="h-[90%] w-0.5 bg-dark-900 flex items-center my-3"></div>
+
     <div class="h-full flex-1 flex justify-center items-center">
       <img src="src/lib/images/ada-logo.png" alt="Logo" class="w-32 h-auto" />
     </div>
@@ -39,7 +45,6 @@
     bottom: 0;
     width: 100%;
     height: 100%;
-    z-index: -1;
     background-image: url("src/lib/images/login_background.svg");
   }
 </style>
