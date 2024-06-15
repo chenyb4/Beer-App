3520f81e0c030ae1ec7c61b016987ce428bd2110
Wouter Baltus <439831@student.saxion.nl>
2024-06-15 17:39:52 +0200

    Added tests for UserService.js. Edited UserService.js to ensure database credibility

--- Backend/tests/UserTest.test.js
+++ Backend/tests/UserTest.test.js
@@ -5,13 +5,82 @@ const db = require("../database");
 
 
 beforeAll(async () => {
-    return await db.sequelize.authenticate();
+    await db.sequelize.authenticate();
 })
 beforeEach(async () => {
-    return await databaseSetup.resetTestDatabase();
+    await databaseSetup.resetTestDatabase();
 })
 
-test("getUsers",async ()=> {
-    const users = await userService.getUser(1);
-    return expect(users.username).toBe('example');
+test("getUsersAge",async ()=> {
+    const {users} = await userService.getAllUsers({query: {isLegalAge: true}});
+    expect(users[0].username).toBe('example');
+})
+
+test("getUsersEmail",async ()=> {
+    const {users} = await userService.getAllUsers({query: {email: 'example'}});
+    expect(users.length).toBe(2);
+})
+
+test("getUserById",async ()=> {
+    const user = await userService.getUser(1);
+    expect(user.username).toBe('example');
+})
+
+test("getUserByQr",async ()=> {
+    const user = await userService.getQRUser('iahdfo9q34w8yhrtharfiulhas');
+    expect(user.username).toBe('example');
+})
+
+test("updateUser",async ()=> {
+    const user = await userService.updateUser({id: 1, credits: 30, loggedInUserId: 1});
+    expect(user.credits).toBe(30);
+})
+
+test("createUser",async ()=> {
+    const user = await userService.createUser('bert','bert@example.com',null, "1999-01-01 00:00:00.000");
+    expect(user.username).toBe('bert');
+})
+
+test("deleteUser",async ()=> {
+    const id = 1
+    await userService.deleteUser(id);
+    const deletedUser = await userService.getUser(id)
+    expect(deletedUser).toBe(null)
+})
+
+test("incrementUserCredits",async ()=> {
+    const id = 1
+    const amount = 5
+    const userBefore = await userService.getUser(id)
+    const creditBefore = userBefore.credits
+    await userService.incrementUserCredits(id, amount, 1)
+    const userAfter = await userService.getUser(id)
+    const creditAfter = userAfter.credits
+
+    expect(Number(creditAfter) - Number(creditBefore)).toBe(amount)
+})
+
+test("incrementUserCreditsFail",async ()=> {
+    await expect(userService.incrementUserCredits(1, -5, 1))
+        .rejects
+        .toThrow(Error);
+})
+
+test("decrementUserCredits",async ()=> {
+    const id = 1
+    const amount = 5
+    await userService.updateUser({id, credits: amount})
+    const userBefore = await userService.getUser(id)
+    const creditBefore = userBefore.credits
+    await userService.decrementUserCredits(id, amount, 1)
+    const userAfter = await userService.getUser(id)
+    const creditAfter = userAfter.credits
+
+    expect(Number(creditBefore) - Number(creditAfter)).toBe(amount)
+})
+
+test("decrementUserCreditsFail",async ()=> {
+    await expect(userService.decrementUserCredits(1, 5, 1))
+        .rejects
+        .toThrow(Error);
 })
