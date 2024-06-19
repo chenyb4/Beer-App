751a241e10d76c0b82f0907c072297d180af4772
Hidde <510739@student.saxion.nl>
2024-06-19 16:02:16 +0200

    transaction update

--- Frontend/src/lib/service/transactions.js
+++ Frontend/src/lib/service/transactions.js
@@ -2,7 +2,7 @@ import {request} from "$lib/service/config.js";
 
 export async function getAllOrders(){
     try {
-        const response = await request( `/orders`, "GET", true);
+        const response = await request( `/orders`, "GET", '',true);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -14,7 +14,7 @@ export async function getAllOrders(){
 
 export async function getOneOrderById(orderId){
     try {
-        const response = await request( `/orders?id=${orderId}`, "GET", true);
+        const response = await request( `/orders?id=${orderId}`, "GET", '',true);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
